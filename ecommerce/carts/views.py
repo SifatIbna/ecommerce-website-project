7426cc65d3bbb2688ee020af107434b91367681c
Sifat <sifatibna.amin9@gmail.com>
2020-06-10 21:21:00 +0600

    worked with sessions

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Cart
+
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    print("New Cart Created")
+    return cart_obj
 
 def cart_home(request):
+    #del request.session['cart_id']
+
     cart_id = request.session.get("cart_id",None)
 
-    if cart_id is None:
-        print('get new cart id')
-        request.session['cart_id'] = 12
-    else:
-        print('Cart ID exists')
+    ''' if cart_id is None:
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id 
         
+    else: '''
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        cart_obj = qs.first()
+        print('Cart ID exists')
+    else:
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id
     # print(request.session)
     # print(dir(request.session))
 
@@ -23,7 +37,7 @@ def cart_home(request):
     # request.session['first_name'] = "Sifat" # SET
     # request.session['user'] = request.user # will show error because in session you can not save objects but
     # request.session['user'] = request.user.username # is okay cause it only saving string
-    
+
     return render(request,"carts/home.html",{})
 
 

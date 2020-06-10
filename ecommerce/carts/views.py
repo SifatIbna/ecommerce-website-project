ed758289f88d351f68539895a1edd521f8bd6fa5
Sifat <sifatibna.amin9@gmail.com>
2020-06-10 21:37:28 +0600

    added cart model manager in models.py

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Cart
 
-def cart_create(user=None):
+''' def cart_create(user=None):
     cart_obj = Cart.objects.create(user=None)
     print("New Cart Created")
     return cart_obj
+ '''
 
 def cart_home(request):
     #del request.session['cart_id']
@@ -18,13 +19,24 @@ def cart_home(request):
         request.session['cart_id'] = cart_obj.id 
         
     else: '''
+
     qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
         cart_obj = qs.first()
-        print('Cart ID exists')
+        
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
+
+        #redundant if cases, cause we did it in models.py
+
     else:
-        cart_obj = cart_create()
+        cart_obj = Cart.objects.new(user=request.user)
         request.session['cart_id'] = cart_obj.id
+
+    return render(request,"carts/home.html",{})
+
+
     # print(request.session)
     # print(dir(request.session))
 
@@ -38,7 +50,7 @@ def cart_home(request):
     # request.session['user'] = request.user # will show error because in session you can not save objects but
     # request.session['user'] = request.user.username # is okay cause it only saving string
 
-    return render(request,"carts/home.html",{})
+    
 
 
     #'session_key', 'set_expiry'

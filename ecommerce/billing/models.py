81ec4f18d8205b23c87153513efa4b23fac2bca0
Sifat <sifatibna.amin9@gmail.com>
2020-06-14 02:39:35 +0600

    added billingModel Manager

--- ecommerce/billing/models.py
+++ ecommerce/billing/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
+from accounts.models import GuestEmail
 
 User = settings.AUTH_USER_MODEL
+
+class BillingProfileManager(models.Manager):
+    def now_or_get(self,request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        
+        created = False
+        obj = None
+
+        if user.is_authenticated:
+            obj, created = self.model.objects.get_or_create(
+                                        user=user, 
+                                        email = user.email
+                                    ) 
+        elif guest_email_id is not None:
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                                                email=guest_email_obj.email)
+        else:
+            pass
+        return obj, created
 
 class BillingProfile(models.Model):
     user             = models.OneToOneField(User,null=True, blank=True, on_delete=models.DO_NOTHING)
@@ -12,6 +34,7 @@ class BillingProfile(models.Model):
     timestamp        = models.DateTimeField(auto_now_add=True)
 
     #customer id in stripe or Braintree
+    objects = BillingProfileManager()
 
     def __str__(self):
         return self.email

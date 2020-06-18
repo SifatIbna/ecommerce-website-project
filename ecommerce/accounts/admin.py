fd624e2e366a45978485436bc38aab05f4f18f0e
Sifat <sifatibna.amin9@gmail.com>
2020-06-18 18:53:29 +0600

    updated admin page

--- ecommerce/accounts/admin.py
+++ ecommerce/accounts/admin.py
@@ -1,13 +1,52 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from .forms import UserAdminChangeForm,UserAdminCreationForm
 from .models import GuestEmail
 
 User = get_user_model()
 
-class UserAdmin(admin.ModelAdmin):
-    search_fields = ['email']
-    class Meta:
-        model = User
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
 
-admin.site.register(User,UserAdmin)
-admin.site.register(GuestEmail)
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'admin','active')
+    list_filter = ('admin','staff','active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('admin','staff','active')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+
+
+
+# Remove Group Model from admin. We're not using it.
+admin.site.unregister(Group)
+
+class GuestEmailAdmin(admin.ModelAdmin):
+    search_field = ['email']
+    class Meta:
+        model = GuestEmail
+
+admin.site.register(GuestEmail,GuestEmailAdmin)

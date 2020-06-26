09dc2c95a978f2c608b731eb74552a146608e5df
Sifat <sifatibna.amin9@gmail.com>
2020-06-26 10:04:09 +0600

    created card model and saved card response

--- ecommerce/billing/views.py
+++ ecommerce/billing/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import JsonResponse,HttpResponse
 from django.utils.http import is_safe_url
 
-from .models import BillingProfile
+from .models import BillingProfile,Card
 
 # Create your views here.
 import stripe
@@ -36,7 +36,9 @@ def payment_method_createview(request):
         if token is not None:
             customer = stripe.Customer.retrieve(billing_profile.customer_id)
             card_response = customer.sources.create(source = token)
-            print(card_response)
+            new_card_obj = Card.objects.add_new(billing_profile, card_response)
+
+            print(new_card_obj)
         # print(request.POST)
         return JsonResponse({"message":"Success! Your Card was added"})
     return HttpResponse("error", status_code=401)
